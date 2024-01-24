de05c7ea31f5a5a308f094f4a07d900d0916e422
jaehwan <dhwoghks0223@gmail.com>
2024-01-24 09:19:43 +0900

    상세페이지 만들고 각각 링크달아줌

--- app/list/page.js
+++ app/list/page.js
@@ -1,5 +1,5 @@
 import { connectDB } from "@/util/database"
-
+import Link from 'next/link'
 
 export default async function List() {
 
@@ -12,11 +12,11 @@ export default async function List() {
     return (
       <div className="list-bg">
         {
-          result.map((doc,data) =>{
+          result.map((data,num) =>{
             return (
-              <div className="list-item">
-                <h4>{doc.title}</h4>
-                <p>{doc.contents}</p>
+              <div className="list-item" key={num}>
+                <Link href={"http://localhost:3000/detail/"+data._id}>{data.title}</Link>
+                <p>{data.contents}</p>
               </div>
             )
           })
