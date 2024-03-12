347d6fa5c607d762b873d77b251cdacf8dd39dce
jaehwan <dhwoghks0223@gmail.com>
2024-03-12 16:23:10 +0900

    api기능 만들어서 댓글목록 불러오기

--- app/detail/[id]/Comment.js
+++ app/detail/[id]/Comment.js
@@ -1,9 +1,17 @@
 'use client'
-import {useState} from 'react'
+import {useEffect, useState} from 'react'
 
 
 export default function Comment(props) {
     let [comment, setComment] = useState('')
+    let [data, setData] = useState([])
+
+  useEffect(()=>{
+    fetch('/api/comment/list?id='+ props._id).then(r=>r.json()).then((result)=>{
+      setData(result)
+    })
+  },[]) 
+
     return (
         <div>
             <div>댓글목록</div>
@@ -12,6 +20,13 @@ export default function Comment(props) {
                 fetch('/api/comment/new', {method : 'POST', body: JSON.stringify({comment: comment, _id: props._id})
                 })
             }}>댓글전송</button>
+            { 
+           data.length > 0 ?
+           data.map((a,i)=>
+             <p key={i}>{a.content}</p>
+           )
+           : '댓글없음'
+        }
         </div>
     )
 } 
