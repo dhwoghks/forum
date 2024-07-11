21e9269068124bb66ab0bae46beaa98a580f6438
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2024-07-11 23:41:22 +0900

    home page에 스포티파이 노래 집어넣음 히히

--- app/page.js
+++ app/page.js
@@ -8,8 +8,17 @@ export default async function Home() {
   let result = await db.collection('post').find().toArray();
   // console.log(result);
   return (
-    <div>
-      <object data = "https://google.com" className = 'game'></object>
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '100vh', marginTop: '20px' }}>
+      <iframe 
+        style={{ borderRadius: "12px" }} 
+        src="https://open.spotify.com/embed/track/5EcCFzo4PyxTYQsrHGL9xU?utm_source=generator" 
+        width="50%" 
+        height="352" 
+        frameborder="0" 
+        allowFullScreen
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" 
+        loading="lazy">
+      </iframe>
     </div>
   )
 }
