05beab711fdf0486408c65d85bfa667f79f36675
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2024-09-24 13:09:52 +0900

    google login create

--- components/LoginBtn.js
+++ components/LoginBtn.js
@@ -3,5 +3,5 @@ import { signIn } from 'next-auth/react'
 const redirectUri = process.env.REDIRECT_URI;
 
 export default function LoginBtn() {
-  return <button class="dropdown" onClick={() => { signIn('github', { callbackUrl: redirectUri }) }}>로그인</button>
+  return <button class="dropdown" onClick={() => { signIn() }}>로그인</button>
 } 
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import { connectDB } from "@/util/database";
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt';
 
@@ -10,6 +11,10 @@ export const authOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
 
     CredentialsProvider({
