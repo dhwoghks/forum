ece171cbb50c2cd36386ee119c440866e1536743
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2024-09-25 17:21:25 +0900

    네이버가 ㅇ니라 카카오로 바꿈

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,6 +4,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import NaverProvider from "next-auth/providers/naver";
+import KakaoProvider from "next-auth/providers/kakao";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from 'bcrypt';
 
@@ -21,9 +22,9 @@ export const authOptions = {
       allowDangerousEmailAccountLinking: true,
 
     }),
-    NaverProvider({
-      clientId: process.env.NAVER_CLIENT_ID,
-      clientSecret: process.env.NAVER_CLIENT_SECRET,
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET,
       allowDangerousEmailAccountLinking: true,
     }),
 
