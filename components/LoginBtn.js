e66d4c2e8cfa00bda19f9c2386c03707ddb359d7
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2024-09-25 16:45:38 +0900

    className

--- components/LoginBtn.js
+++ components/LoginBtn.js
@@ -2,5 +2,5 @@
 import { signIn } from 'next-auth/react'
 
 export default function LoginBtn() {
-  return <button class="dropdown" onClick={() => { signIn() }}>로그인</button>
+  return <button className="dropdown" onClick={() => { signIn() }}>로그인</button>
 } 
