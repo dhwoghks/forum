8469e87a9b5e25b535d02e177a502b8cff0d9046
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2024-09-25 10:47:53 +0900

    쓸데없는 redirecturi 삭제함

--- components/LoginBtn.js
+++ components/LoginBtn.js
@@ -1,6 +1,5 @@
 'use client';
 import { signIn } from 'next-auth/react'
-const redirectUri = process.env.REDIRECT_URI;
 
 export default function LoginBtn() {
   return <button class="dropdown" onClick={() => { signIn() }}>로그인</button>
