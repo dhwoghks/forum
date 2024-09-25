7d1cf7e17b5211a3492645ef86256281baefd98c
dhwoghks <111697672+dhwoghks@users.noreply.github.com>
2024-09-25 10:54:42 +0900

    깃허브 구글 이메일 중복 허용

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,7 @@ export const authOptions = {
       clientId: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
       redirectUri: process.env.GITHUB_REDIRECT_URI,
+      allowDangerousEmailAccountLinking: true,
 
     }),
     GoogleProvider({
